9afd9a224228bab332f3f41e686dc88d35f8253c
joao costa <joaovitorribeiroc1@gmail.com>
2022-08-10 13:45:51 -0300

    cards

--- src/components/CardProject/index.js
+++ src/components/CardProject/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import "./style/cardProject.css";
 import animeJox from "../../assets/images/anime-jox.png";
 import { FiExternalLink } from "react-icons/fi";
+import { BsImages } from "react-icons/bs";
 
 const CardProject = (props) => {
   const redirectTo = (link) => {
@@ -10,19 +11,13 @@ const CardProject = (props) => {
   return (
     <div className="cardContainer">
       <h2 className="cardTitle">{props.name}</h2>
-      <div className="imgCardContainer">
-        <img className="imgCard" src={animeJox} />
-        <div className="imgCardLink" onClick={() => redirectTo(props.link)}>
-          <p>Visitar</p>
-          <FiExternalLink style={{ paddingTop: 2, paddingLeft: 2 }} />
-        </div>
-        <div className="imgCardDescription">
+      <div>
+        <div className="CardDescription">
           <p>{props.description}</p>
         </div>
-      </div>
-      <div>
+
         <div>
-          <h3>Tecnologias:</h3>
+          <h3>{"{ " + "Tecnologias" + " }"}</h3>
           <div className="tecList">
             {props.technologies.map((res, index) => {
               const handleComma = index + 1;
@@ -36,6 +31,17 @@ const CardProject = (props) => {
           </div>
         </div>
       </div>
+      <span className="gallery">
+        <h3>
+          Geleria <BsImages />
+        </h3>
+      </span>
+      <span className="visitPage">
+        <p>
+          Visitar
+          <FiExternalLink style={{ paddingTop: 2, paddingLeft: 2 }} />
+        </p>
+      </span>
     </div>
   );
 };
